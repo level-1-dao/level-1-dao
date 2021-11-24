b5ef83fef7e89d595a09e8bf493cfbe1f51df20a
Ray KananI <hello@raykanani.com>
2021-11-23 19:12:13 -0500

    add packages

--- next.config.js
+++ next.config.js
@@ -11,4 +11,7 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  images: {
+    domains: ['tailwindui.com'],
+  },
 });

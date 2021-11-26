41bc0f614769d81a20d03a4fd349481e3f7a3525
Ray KananI <hello@raykanani.com>
2021-11-26 13:47:52 -0500

    Revert "Merge branch 'init-boilerplate' of https://github.com/atj3097/level-1-dao into init-boilerplate"
    This reverts commit 28b498d68b7086a77de4930a12e530b1c9ae56a2, reversing
    changes made to 45ee384a59db293fcf94856ecf69bb5bcf1543a6.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from 'next/app';
+
+import '../styles/main.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;

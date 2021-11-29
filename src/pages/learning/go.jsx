dfbcd91e1093239039d929056d2030517ec1205c
Ray KananI <hello@raykanani.com>
2021-11-29 11:22:18 -0500

    add curriculum stepper function

--- src/pages/learning/go.jsx
+++ src/pages/learning/go.jsx
@@ -1,31 +1,71 @@
-import { Meta } from '../../layout/Meta.tsx';
+import {useState} from 'react';
+import {Meta} from '../../layout/Meta.tsx';
 import AppPageTwoColumn from '../../layout/AppPageTwoColumn';
 import LearningModule from '../../templates/LearningModule';
 import Curriculum from '../../components/Curriculum';
 
 import CurriculumData from '../../mockData/CurriculumData';
 
-const LearningLandingPage = () => (
-  <div className="h-full">
-    <AppPageTwoColumn
-      meta={
-        <Meta
-          title="Level1"
-          description="A fun and friendly space for DAO onboarding"
-        />
-      }
-      leftColumn={<LearningModule />}
-      rightColumn={
-        <div className="flex flex-col space-y-4 items-center w-full px-4">
-          <button className="btn btn-accent btn-outline text-left w-full">
-            L1 tokens collected:
-            <div className="badge ml-2">18</div>
-          </button>
-          <Curriculum active={1} curriculum={CurriculumData} />
-        </div>
-      }
-    />
-  </div>
-);
+const LearningLandingPage = () => {
+  const [curriculumProgress, setCurriculumProgress] = useState(1);
+
+  const handleCurriculumProgress = (progress) => {
+    setCurriculumProgress(progress);
+  };
+
+  const curriculumSize = CurriculumData.length;
+
+  return (
+    <div className="h-full">
+      <AppPageTwoColumn
+        meta={
+          <Meta
+            title="Level1"
+            description="A fun and friendly space for DAO onboarding"
+          />
+        }
+        leftColumn={<LearningModule />}
+        rightColumn={
+          <div className="flex flex-col space-y-4 items-center w-full px-4">
+            <button className="btn btn-accent btn-outline text-left w-full">
+              L1 tokens collected:
+              <div className="badge ml-2">18</div>
+            </button>
+            {/* Temporary Curriculum Progress Stepper UI */}
+            {curriculumProgress < curriculumSize ? (
+              <div className="flex flex-col space-y-2">
+                <div className="flex items-center">
+                  <div
+                    className="btn"
+                    onClick={() =>
+                      handleCurriculumProgress(curriculumProgress + 1)
+                    }
+                  >
+                    Progress to step {curriculumProgress + 1}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="flex flex-col space-y-2">
+                <div className="flex items-center">
+                  <div
+                    className="btn"
+                    onClick={() => alert('You finished learning module')}
+                  >
+                    Finish learning module
+                  </div>
+                </div>
+              </div>
+            )}
+            <Curriculum
+              active={curriculumProgress}
+              curriculum={CurriculumData}
+            />
+          </div>
+        }
+      />
+    </div>
+  );
+};
 
 export default LearningLandingPage;

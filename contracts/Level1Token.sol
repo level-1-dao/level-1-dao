aa5e21b5a5264c29e88d273f6ff605dcbf134b17
atj3097 <adamthomasjackson3@gmail.com>
2021-11-30 12:00:28 -0500

    Token contract is now ownable for whichever wallet deploys this account.

--- contracts/Level1Token.sol
+++ contracts/Level1Token.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LevelOne is ERC20 {
+contract LevelOne is ERC20, Ownable {
         constructor() ERC20("LevelOne", "LO") { 
             _mint(msg.sender, 1000000); 
         } 

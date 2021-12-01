f2798f96aaa986e8e567f6adb160800d924f61de
atj3097 <adamthomasjackson3@gmail.com>
2021-11-30 21:37:56 -0500

    Learn2Earn.sol draft

---
+++ contracts/Learn2Earn.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.10;   
+
+contract Learn2Earn {  
+    //internal values
+    // levelone token smart contract address  
+    address private levelOneContract; //add the actual test net address
+
+    // nft smart contract address 
+    address private levelOneNftContract; // add the actual testnet address
+
+    //functions 
+    //award user(currentAddress, amountOfTokens)  
+    function awardUser(address learner, uint256 tokenAmount) public returns (uint256) { 
+
+    }
+
+    // awardNft(currentAddress, nft)  This may not be neccessary. User could just directly mint from the smart contract.  
+
+    //check if user is apart of a dao. just take gitcoin
+    function isDaoHolder(address learner, address daoToken) public returns (bool) { 
+
+    }
+
+    function hasLevelOneToken(address learner) public { 
+
+    }
+
+    function hasLevelOneNft(address learner) public { 
+
+    }
+}

708648b789b71fa0ce742eae295f0bf0e8e74777
Ray KananI <hello@raykanani.com>
2021-12-02 00:26:07 -0500

    add wallet connect slideout

--- src/components/Header/Slideout.jsx
+++ src/components/Header/Slideout.jsx
@@ -101,87 +101,41 @@ const Slideout = ({open, setOpen, walletConnected}) => {
                         ))}
                       </ul>
                     ) : (
-                      <div className="pb-16 space-y-6">
+                      <div className="space-y-6">
                         <div>
-                          <div className="block w-full aspect-w-10 aspect-h-7 rounded-lg overflow-hidden">
-                            <img
-                              src="https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80"
-                              alt=""
-                              className="object-cover"
-                            />
-                          </div>
-                          <div className="mt-4 flex items-start justify-between">
-                            <div>
-                              <h2 className="text-lg font-medium text-gray-900">
-                                <span className="sr-only">Details for </span>
-                                IMG_4985.HEIC
-                              </h2>
-                              <p className="text-sm font-medium text-gray-500">
-                                3.9 MB
-                              </p>
-                            </div>
-                            <button
-                              type="button"
-                              className="ml-4 h-8 w-8 bg-white rounded-full flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                            >
-                              <HeartIcon
-                                className="h-6 w-6"
-                                aria-hidden="true"
-                              />
-                              <span className="sr-only">Favorite</span>
-                            </button>
-                          </div>
+                          <p className="text-sm font-medium text-gray-500">
+                            Wallet address:
+                          </p>
+                          <h2 className="text-base font-medium text-gray-900 break-words">
+                            <span className="sr-only">Wallet address </span>
+                            0xF6f15f6007F0703b4013317A1ee80ad4040Cf98E
+                          </h2>
+                        </div>
+                        <div className="h-40 w-40 overflow-hidden">
+                          <img
+                            src="/assets/images/coins.png"
+                            alt="tokens image"
+                            className="object-cover"
+                          />
                         </div>
                         <div>
                           <h3 className="font-medium text-gray-900">
-                            Information
+                            Tokens collected:
                           </h3>
                           <dl className="mt-2 border-t border-b border-gray-200 divide-y divide-gray-200">
                             <div className="py-3 flex justify-between text-sm font-medium">
-                              <dt className="text-gray-500">Uploaded by</dt>
-                              <dd className="text-gray-900">Marie Culver</dd>
+                              <dt className="text-gray-500">Level1 Tokens</dt>
+                              <dd className="text-gray-900">1,238</dd>
                             </div>
                             <div className="py-3 flex justify-between text-sm font-medium">
-                              <dt className="text-gray-500">Created</dt>
-                              <dd className="text-gray-900">June 8, 2020</dd>
-                            </div>
-                            <div className="py-3 flex justify-between text-sm font-medium">
-                              <dt className="text-gray-500">Last modified</dt>
-                              <dd className="text-gray-900">June 8, 2020</dd>
-                            </div>
-                            <div className="py-3 flex justify-between text-sm font-medium">
-                              <dt className="text-gray-500">Dimensions</dt>
-                              <dd className="text-gray-900">4032 x 3024</dd>
-                            </div>
-                            <div className="py-3 flex justify-between text-sm font-medium">
-                              <dt className="text-gray-500">Resolution</dt>
-                              <dd className="text-gray-900">72 x 72</dd>
+                              <dt className="text-gray-500">Completion NFTs</dt>
+                              <dd className="text-gray-900">22</dd>
                             </div>
                           </dl>
                         </div>
                         <div>
                           <h3 className="font-medium text-gray-900">
-                            Description
-                          </h3>
-                          <div className="mt-2 flex items-center justify-between">
-                            <p className="text-sm text-gray-500 italic">
-                              Add a description to this image.
-                            </p>
-                            <button
-                              type="button"
-                              className="-mr-2 h-8 w-8 bg-white rounded-full flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                            >
-                              <PencilIcon
-                                className="h-5 w-5"
-                                aria-hidden="true"
-                              />
-                              <span className="sr-only">Add description</span>
-                            </button>
-                          </div>
-                        </div>
-                        <div>
-                          <h3 className="font-medium text-gray-900">
-                            Shared with
+                            Learned with
                           </h3>
                           <ul
                             role="list"
@@ -198,13 +152,6 @@ const Slideout = ({open, setOpen, walletConnected}) => {
                                   Aimee Douglas
                                 </p>
                               </div>
-                              <button
-                                type="button"
-                                className="ml-6 bg-white rounded-md text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                              >
-                                Remove
-                                <span className="sr-only"> Aimee Douglas</span>
-                              </button>
                             </li>
                             <li className="py-3 flex justify-between items-center">
                               <div className="flex items-center">
@@ -217,48 +164,8 @@ const Slideout = ({open, setOpen, walletConnected}) => {
                                   Andrea McMillan
                                 </p>
                               </div>
-                              <button
-                                type="button"
-                                className="ml-6 bg-white rounded-md text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                              >
-                                Remove
-                                <span className="sr-only">
-                                  {' '}
-                                  Andrea McMillan
-                                </span>
-                              </button>
-                            </li>
-                            <li className="py-2 flex justify-between items-center">
-                              <button
-                                type="button"
-                                className="group -ml-1 bg-white p-1 rounded-md flex items-center focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                              >
-                                <span className="w-8 h-8 rounded-full border-2 border-dashed border-gray-300 flex items-center justify-center text-gray-400">
-                                  <PlusSmIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                                <span className="ml-4 text-sm font-medium text-indigo-600 group-hover:text-indigo-500">
-                                  Share
-                                </span>
-                              </button>
                             </li>
                           </ul>
-                        </div>
-                        <div className="flex">
-                          <button
-                            type="button"
-                            className="flex-1 bg-indigo-600 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                          >
-                            Download
-                          </button>
-                          <button
-                            type="button"
-                            className="flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                          >
-                            Delete
-                          </button>
                         </div>
                       </div>
                     )}

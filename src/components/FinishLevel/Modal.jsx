1b23c3f5d78a1def1e562ae05bf260c5b3ffcaae
Ray KananI <hello@raykanani.com>
2021-12-10 22:20:39 -0500

    add Rinkeby disclaimer

--- src/components/FinishLevel/Modal.jsx
+++ src/components/FinishLevel/Modal.jsx
@@ -100,6 +100,27 @@ const PopUp = ({
                 </div>
               </div>
               <div className="space-y-2 mt-5 sm:mt-6">
+                <div className="alert alert-warning">
+                  <div className="flex-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="w-6 h-6 mx-2 stroke-current"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                      ></path>
+                    </svg>
+                    <label>
+                      Make sure your wallet is connected to the Rinkeby Test
+                      Network for demo.
+                    </label>
+                  </div>
+                </div>
                 <button
                   type="button"
                   className={

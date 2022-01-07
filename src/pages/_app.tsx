15a246a323a0dfd672aa7f2163c12832cb0fb4a1
Muhammad Raza <faisalraza32@gmail.com>
2022-01-07 18:03:13 -0500

    Wrap pages with auth0 UserProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import {AppProps} from 'next/app';
+import { AppProps } from 'next/app';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 import '../styles/main.css';
 
-const MyApp = ({Component, pageProps}: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
+};
 
 export default MyApp;

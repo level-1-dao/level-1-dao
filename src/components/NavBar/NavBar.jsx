bc7b17c2a4f01513c118f6fc96014cc017f1898d
Muhammad Raza <faisalraza32@gmail.com>
2022-01-07 18:08:45 -0500

    Implement user registration and authentication

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,11 @@
 import {Fragment, useState} from 'react';
 import {Disclosure, Menu, Transition} from '@headlessui/react';
-import {SparklesIcon, MenuIcon, XIcon} from '@heroicons/react/outline';
+import { SparklesIcon, MenuIcon, XIcon, LoginIcon } from '@heroicons/react/outline';
+import { useUser } from '@auth0/nextjs-auth0';
 import Link from 'next/link';
+import Image from 'next/image';
 import Slideout from '../NavBar/Slideout';
+import {useRouter} from 'next/router';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -11,6 +14,8 @@ function classNames(...classes) {
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [walletConnected, setWalletConnected] = useState(false);
+  const { user, isLoading } = useUser();
+  const router = useRouter();
 
   const connectWallet = () => {
     setWalletConnected(true);
@@ -75,71 +80,88 @@ const NavBar = () => {
                     <SparklesIcon className="h-6 w-6" aria-hidden="true" />
                   </button>
                 </div>
-
                 {/* Profile dropdown */}
+                {user
+                  ? (
                 <Menu as="div" className="ml-4 relative flex-shrink-0">
-                  <div>
-                    <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="z-40 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({active}) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({active}) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({active}) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                <div>
+                  <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <span className="sr-only">Open user menu</span>
+                    <Image
+                      className="h-8 w-8 rounded-full"
+                      src={user.picture}
+                      alt=""                        
+                      height="32px"
+                      width="32px"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="z-40 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                      {({active}) => (
+                        <a
+                          href="/profile"
+                          className={classNames(
+                            active ? 'bg-gray-100' : '',
+                            'block px-4 py-2 text-sm text-gray-700'
+                          )}
+                        >
+                          Your Profile
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({active}) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            active ? 'bg-gray-100' : '',
+                            'block px-4 py-2 text-sm text-gray-700'
+                          )}
+                        >
+                          Settings
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({active}) => (
+                        <a
+                          href="/api/auth/logout"
+                          className={classNames(
+                            active ? 'bg-gray-100' : '',
+                            'block px-4 py-2 text-sm text-gray-700'
+                          )}
+                        >
+                          Sign out
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+                  )
+                  : (
+                    <div className="ml-4 relative flex-shrink-0">
+                      <button
+                        type="button"
+                        className="flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => router.push("/api/auth/login")}
+                      >
+                        <span className="sr-only">Sign In</span>
+                        <LoginIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+                    </div>
+                  )
+                }        
               </div>
             </div>
           </div>
@@ -147,21 +169,38 @@ const NavBar = () => {
           <Disclosure.Panel className="lg:hidden">
             <div className="pt-4 pb-3 border-t border-gray-200">
               <div className="flex items-center px-4">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-gray-800">
-                    Tom Cook
+                {user ? (<>
+                  <div className="flex-shrink-0">
+                    <Image
+                      className="h-10 w-10 rounded-full"
+                      src={user.picture}
+                      alt=""
+                      height="32px"
+                      width="32px"
+                    />
                   </div>
-                  <div className="text-sm font-medium text-gray-500">
-                    tom@example.com
+                  <div className="ml-3">
+                    <div className="text-base font-medium text-gray-800">
+                      {user.name}
+                    </div>
+                    <div className="text-sm font-medium text-gray-500">
+                      {user.email}
+                    </div>
                   </div>
+                </>
+                ) : (
+                  <div className="ml-4 relative flex-shrink-0">
+                  <button
+                    type="button"
+                    className="flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={() => router.push("/api/auth/login")}
+                  >
+                    <span className="sr-only">Sign In</span>
+                    <LoginIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
                 </div>
+                )}
+                
                 <button
                   type="button"
                   className="ml-auto flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -170,11 +209,12 @@ const NavBar = () => {
                   <span className="sr-only">Your wallet</span>
                   <SparklesIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
-              </div>
+              </div>              
+              {user && (
               <div className="mt-3 space-y-1">
                 <Disclosure.Button
                   as="a"
-                  href="#"
+                  href="/profile"
                   className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
                 >
                   Your Profile
@@ -188,12 +228,13 @@ const NavBar = () => {
                 </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
-                  href="#"
+                  href="/api/auth/logout"
                   className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
                 >
                   Sign out
                 </Disclosure.Button>
-              </div>
+                </div>
+                )}
             </div>
           </Disclosure.Panel>
           {/* Slideout */}

301f80c00bcd37c821596702b2c8a0fe56ea7512
Muhammad Raza <faisalraza32@gmail.com>
2022-01-11 18:54:05 -0500

    Create hoc with apollo client to use query for graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import { AppProps } from 'next/app';
-import { UserProvider } from '@auth0/nextjs-auth0';
-
-import '../styles/main.css';
+import { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import { withApollo } from "../lib/withApollo";
+import "../styles/main.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
-  )
+  );
 };
 
-export default MyApp;
+export default withApollo({ ssr: true })(MyApp);

b1b1ba3119a050e5fa9b757f43240c949634599b
atj3097 <adamthomasjackson3@gmail.com>
2022-01-21 06:52:15 -0500

    Adding some new dependencies

--- contracts/Level1NFT.sol
+++ contracts/Level1NFT.sol
@@ -2,14 +2,30 @@
 pragma solidity ^0.8.10;  
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; 
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Counters.sol"; 
+import "@opengsn/gsn/contracts/BaseRelayRecipient.sol";
 
-contract Level1Completion is ERC721URIStorage {  
+contract Level1Completion is ERC721URIStorage, BaseRelayRecipient {  
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds; 
 
-    constructor() ERC721("L1Completion","L1C") {}  
+    constructor(address _trustedForwarder) ERC721("L1Completion","L1C") { 
+        trustedForwarder = _trustedForwarder;
+    }   
+
+    modifier onlyOwner(){
+            require(msg.sender == owner);
+            _;
+        }
+
+    function setTrustedForwarder(address _trustedForwarder) onlyOwner { 
+       trustedForwarder = _trustedForwarder;
+    }
+
+    function versionRecipient() external view override returns (string memory) {
+        return "1";
+    }
 
     function awardCertificate(address learner, string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();

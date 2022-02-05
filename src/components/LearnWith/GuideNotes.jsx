e69d739c37d69bcea852f039799f8be752894963
Ray KananI <hello@raykanani.com>
2022-02-04 19:28:14 -0500

    make guide notes responsive

--- src/components/LearnWith/GuideNotes.jsx
+++ src/components/LearnWith/GuideNotes.jsx
@@ -1,19 +1,19 @@
-import {ChatAltIcon, HandIcon} from '@heroicons/react/outline';
+import { ChatAltIcon, HandIcon } from "@heroicons/react/outline";
 
 const people = [
   {
-    name: 'Leslie Alexander',
-    role: 'Gitcoin Community Manager',
+    name: "Leslie Alexander",
+    role: "Gitcoin Community Manager",
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    bio: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.",
+    twitterUrl: "#",
+    linkedinUrl: "#",
   },
   // More people...
 ];
 
-const GuideNotes = ({notes}) => {
+const GuideNotes = ({ notes }) => {
   return (
     <div className="mx-auto max-w-7xl">
       <div className="space-y-12 lg:gap-8">
@@ -27,31 +27,31 @@ const GuideNotes = ({notes}) => {
           >
             {people.map((person) => (
               <li key={person.name}>
-                <div className="sm:grid sm:grid-cols-4 sm:items-start sm:gap-6 sm:space-y-0">
-                  <div className="image-container sm:col-span-1 space-y-4 max-w-xs">
-                    <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
-                      <img
-                        className="object-cover shadow-lg rounded-lg"
-                        src={person.imageUrl}
-                        alt=""
-                      />
+                <div className="sm:grid sm:grid-cols-5 sm:items-start sm:gap-6 sm:space-y-2">
+                  <div className="image-container space-y-2 mb-4 sm:col-span-1 grid-cols-2 gap-2 max-w-xs flex sm:flex-col">
+                    <div className="avatar col-span-1">
+                      <div className="rounded-full w-24 h-24">
+                        <img src={person.imageUrl} alt="Guide's avatar" />
+                      </div>
                     </div>
-                    <div className="leading-6 space-y-1">
-                      <h3 className="font-medium text-primary text-lg">
-                        {person.name}
-                      </h3>
-                      <p className="text">{person.role}</p>
+                    <div className="guide-info col-span-1 space-y-2">
+                      <div className="leading-6">
+                        <h3 className="font-medium text-primary text-lg">
+                          {person.name}
+                        </h3>
+                        <p className="text">{person.role}</p>
+                      </div>
+                      <button className="btn btn-xs btn-outline btn-accent">
+                        Message
+                        <ChatAltIcon className="ml-2 h-4 w-4" />
+                      </button>
+                      <button className="btn btn-xs btn-outline btn-accent">
+                        High Five
+                        <HandIcon className="ml-2 h-4 w-4" />
+                      </button>
                     </div>
-                    <button className="btn btn-xs btn-outline btn-accent">
-                      Message
-                      <ChatAltIcon className="ml-2 h-4 w-4" />
-                    </button>
-                    <button className="btn btn-xs btn-outline btn-accent">
-                      High Five
-                      <HandIcon className="ml-2 h-4 w-4" />
-                    </button>
                   </div>
-                  <div className="sm:col-span-3">
+                  <div className="sm:col-span-4">
                     <div className="space-y-4">
                       <div className="text-xl">
                         <p className="">{notes}</p>

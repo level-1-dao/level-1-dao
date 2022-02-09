9ed9a1e46288d335017f790a9842a6cf675b8549
Ray KananI <hello@raykanani.com>
2022-02-08 22:45:47 -0500

    subscribe on component mount vs subscribing on every change

--- src/components/LearnWith/Input.jsx
+++ src/components/LearnWith/Input.jsx
@@ -22,7 +22,7 @@ const Input = ({ learningBitId }) => {
 
   useEffect(() => {
     subscribeToLearningMoments();
-  });
+  }, []);
 
   const subscribeToLearningMoments = () => {
     subscribeToMore({

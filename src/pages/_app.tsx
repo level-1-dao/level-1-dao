d9df27ea8ab88e37ff9a05bdbaf13191c2761df9
Muhammad Raza <faisalraza32@gmail.com>
2022-02-25 22:39:54 -0500

    Add token expired event hook to refresh the idToken

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { withApollo } from "../lib/withApollo";
+import useTokenExpiredEvent from "../../hooks/useTokenExpiredEvent";
 import "../styles/main.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useTokenExpiredEvent();
   return (
     <UserProvider>
       <Component {...pageProps} />

d585db1edb29044f721d31b74a03e7c08fa9ec2f
Ray KananI <kananiconsultants@gmail.com>
2022-02-28 14:07:18 -0500

    remove hardcoded people data

--- src/components/LearnWith/GuideNotes.jsx
+++ src/components/LearnWith/GuideNotes.jsx
@@ -1,19 +1,6 @@
 import { Image } from "next/image";
 import { ChatAltIcon } from "@heroicons/react/outline";
 
-const people = [
-  {
-    name: "Leslie Alexander",
-    role: "Gitcoin Community Manager",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    bio: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.",
-    twitterUrl: "#",
-    linkedinUrl: "#",
-  },
-  // More people...
-];
-
 const GuideNotes = ({ guideNoteData }) => {
   return (
     <div className="mx-auto max-w-7xl">

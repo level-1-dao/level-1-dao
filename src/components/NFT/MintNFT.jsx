76f1a5cb2e402e59c50d94be815e33bb91b3c20a
Ray KananI <kananiconsultants@gmail.com>
2022-03-13 14:27:01 -0400

    remove paymaster code

--- src/components/NFT/MintNFT.jsx
+++ src/components/NFT/MintNFT.jsx
@@ -28,21 +28,12 @@ const MintNFT = ({ metaData }) => {
   const [proofOfTxn, setProofOfTxn] = useState();
   const [userSubmittedAddress, setUserSubmittedAddress] = useState("");
   const [poapTokenID, setPAOPTokenID] = useState(0);
-  const [poapTokenURI, setPAOPTokenURI] = useState("");
-  const [balanceUpdated, setBalanceUpdated] = useState(false);
 
   //notifications
   const [loading, setLoading] = useState(false);
   const [relayMessage, setRelayMessage] = useState("");
 
-  //Paymaster
-
-  const [paymasterBalance, setPaymasterBalance] = useState(0);
-  const [whitelistPMAddress, setWhitelistPMAddress] = useState();
-
   //contract Objects
-
-  const [relayHubContractSign, setRelayHubContractSign] = useState();
   const [
     Level1CompletionContractEphemeral,
     setLevel1CompletionContractEphemeral,
@@ -64,8 +55,6 @@ const MintNFT = ({ metaData }) => {
     async function initContract() {
       // all addresses in rinkebyAddresses are contracts already deployed by OpenGSN. Just the addresses are needed.
       // Whitelist Paymaster and Level1 are the contracts that will be deployed by us.
-
-      setWhitelistPMAddress(whitelistDeployed.address);
 
       const gsnConfig = {
         relayLookupWindowBlocks: 1e5,
@@ -117,7 +106,6 @@ const MintNFT = ({ metaData }) => {
         RelayHub.abi,
         ownerWalletWithProvider
       );
-      setRelayHubContractSign(relayContractSign);
 
       //create new instance of Level1Completion contract
       const Level1CompletionAddress = level1CompletionDeployed.address;
@@ -134,22 +122,6 @@ const MintNFT = ({ metaData }) => {
       setLevel1CompletionContractEphemeral(ephemeralContract);
     }
   }, [relayProvider, oneTimeAccount, localHttpProvider]);
-
-  //balance listener
-
-  useEffect(() => {
-    if ((whitelistPMAddress && relayHubContractSign) || balanceUpdated) {
-      updateBalance();
-    }
-
-    async function updateBalance() {
-      const balance = await relayHubContractSign.balanceOf(whitelistPMAddress);
-      const formattedBalance = ethers.utils.formatEther(balance);
-
-      setPaymasterBalance(formattedBalance);
-      setBalanceUpdated(false);
-    }
-  }, [whitelistPMAddress, relayHubContractSign, balanceUpdated]);
 
   //Level1Completiion Contract Function
 
@@ -175,7 +147,6 @@ const MintNFT = ({ metaData }) => {
             const newTokenURI =
               await Level1CompletionContractEphemeral.tokenURI(newTokenID);
             setPAOPTokenID(newTokenID);
-            setPAOPTokenURI(newTokenURI);
             setUserSubmittedAddress("");
             setLoading(false);
             console.log({
@@ -200,94 +171,8 @@ const MintNFT = ({ metaData }) => {
     }
   }
 
-  //   const RandomWallet = () => {
-  //     const [randomAddress, setRandomAddress] = useState("");
-
-  //     function setRandom() {
-  //       const newWallet = ethers.Wallet.createRandom();
-  //       setRandomAddress(newWallet.address);
-  //     }
-
-  //     return (
-  //       <>
-  //         <button className="siteButton" onClick={setRandom}>
-  //           {" "}
-  //           Create random address:
-  //         </button>
-  //         {randomAddress ? <p>{randomAddress}</p> : null}
-  //       </>
-  //     );
-  //   };
-
-  //Paymaster Component
-
-  //   const RefillPaymaster = () => {
-  //     //need to be on deploying account to send
-  //     const [amount, setAmount] = useState(0);
-  //     const [amountMessage, setAmountMessage] = useState("");
-
-  //     useEffect(() => {
-  //       setAmountMessage("");
-  //       setAmount(0);
-  //     }, []);
-
-  //     async function deposit() {
-  //       if (amount > 0) {
-  //         setLoading(true);
-  //         await relayHubContractSign
-  //           .depositFor(whitelistPMAddress, { value: amount.toString() })
-  //           .then((result) => {
-  //             relayProvider.waitForTransaction(result.hash).then((result) => {
-  //               if (result) {
-  //                 setBalanceUpdated(true);
-  //                 setLoading(false);
-  //               }
-  //             });
-  //           })
-  //           .catch((err) => {
-  //             console.log(err, "refill error");
-  //             setLoading(false);
-  //           });
-  //       } else {
-  //         setAmountMessage("enter an amount greater than zero");
-  //       }
-  //     }
-
-  //     return (
-  //       <div style={{ width: "60%" }}>
-  //         <button
-  //           onClick={deposit}
-  //           className="siteButton"
-  //           style={{ fontSize: "30px", width: "70%" }}
-  //         >
-  //           Send to Paymaster
-  //         </button>
-  //         {amountMessage ? <p>{amountMessage}</p> : null}
-
-  //         <input
-  //           name="refillPaymaster"
-  //           type="text"
-  //           placeholder="enter amount to send to paymaster"
-  //           onChange={(e) => setAmount(e.target.value)}
-  //           value={amount || ""}
-  //           style={{
-  //             width: "80%",
-  //             height: "40px",
-  //             fontSize: "20px",
-  //             marginTop: "30px",
-  //             textAlign: "center",
-  //           }}
-  //         />
-  //       </div>
-  //     );
-  //   };
-
   return (
     <div className="container">
-      {/* <RandomWallet /> */}
-
-      <br />
-
       <input
         name="userAddress"
         type="text"
@@ -319,39 +204,12 @@ const MintNFT = ({ metaData }) => {
 
       {poapTokenID ? (
         <>
-          <p style={{ fontWeight: "bold", color: "green" }}>
-            Transaction Success!
-          </p>
-          <p style={{ color: "white" }}>
-            New Token ID: {poapTokenID.toNumber()}
-          </p>
-        </>
-      ) : null}
-      {poapTokenURI ? (
-        <>
-          <p style={{ color: "white" }}>Token Image:</p>
-          <img src={poapTokenURI} alt={"poap token img"} />
-          <p>{poapTokenURI}</p>
+          <p>Transaction Success!</p>
+          <p>New Token ID: {poapTokenID.toNumber()}</p>
         </>
       ) : null}
 
       {errorMessage}
-
-      <p style={{ marginBottom: "30px" }}>
-        Current Balance of Paymaster is: {paymasterBalance} eth
-      </p>
-
-      {/* <hr style={{ color: "white", width: "80%" }} />
-        <h2>Paymaster Interaction</h2>
-        <p className="info-text">
-          Gas fees are paid by a Paymaster contract in the relay system. This
-          contracts balance needs to be maintained by the deployer. Sample
-          function below.
-        </p>
-        <p style={{ marginBottom: "30px" }}>
-          Current Balance of Paymaster is: {paymasterBalance} eth
-        </p>
-        <RefillPaymaster /> */}
     </div>
   );
 };

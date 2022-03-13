9ffa885aef73b93d8e1755dfeb27b51ed4ba2ea1
Ray KananI <kananiconsultants@gmail.com>
2022-03-13 15:38:38 -0400

    remove old minting logic

--- src/templates/LearningJourney/CurriculumSidebar.jsx
+++ src/templates/LearningJourney/CurriculumSidebar.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import { NFT } from "../../components/NFT";
 import PopUp from "../../components/FinishLevel/Modal";
-import { createAlchemyWeb3 } from "@alch/alchemy-web3";
-import Level1Completion from "../../../build/contracts/Level1Completion.json";
-import Learn2Earn from "../../../build/contracts/Learn2Earn.json";
 import Curriculum from "../../components/Curriculum";
 import { Progress } from "../../components/LearningJourney";
 
@@ -16,10 +13,6 @@ const CurriculumSidebar = ({
   user,
 }) => {
   const [finishedJourney, setFinishedJourney] = useState(false);
-  const [minting, setMinting] = useState(false);
-  const [addingTokens, setAddingTokens] = useState(false);
-  const [mintComplete, setMintComplete] = useState(false);
-  const [addTokensComplete, setAddTokensComplete] = useState(false);
   const todoLearningBits = learningBits.length;
   let completedLearningBits = 0;
 
@@ -42,71 +35,6 @@ const CurriculumSidebar = ({
     return false;
   };
 
-  const web3 = createAlchemyWeb3(
-    "https://eth-rinkeby.alchemyapi.io/v2/XW3eK_0nzE7TCKgZ589OxC94gNQrYJyW"
-  );
-
-  const id = web3.eth.net.getId();
-
-  const deployedNetwork = Learn2Earn.networks[4];
-  let learn2EarnInstance = new web3.eth.Contract(
-    Learn2Earn.abi,
-    deployedNetwork.address
-  );
-
-  const level1NFTDeployedNetwork = Level1Completion.networks[4];
-  let level1NFTInstance = new web3.eth.Contract(
-    Level1Completion.abi,
-    level1NFTDeployedNetwork.address
-  );
-
-  async function awardUser() {
-    setAddingTokens(true);
-    try {
-      const learnerAddress = await web3.eth.getAccounts();
-      const levelOneAward = await learn2EarnInstance.methods
-        .awardUser(learnerAddress[0], 90)
-        .send({
-          from: learnerAddress[0],
-        });
-      if (levelOneAward.status) {
-        setAddTokensComplete(true);
-        setAddingTokens(false);
-      }
-      return levelOneAward.status;
-    } catch (error) {
-      console.log(error);
-      setAddingTokens(false);
-      return;
-    }
-    return;
-  }
-
-  async function awardNFT() {
-    setMinting(true);
-    try {
-      const learnerAddress = await web3.eth.getAccounts();
-      const nftAward = await level1NFTInstance.methods
-        .awardCertificate(
-          learnerAddress[0],
-          "https://gateway.pinata.cloud/ipfs/QmaLQ22ExEhxLenfCdu5k3pGUDQPH9bAf4Q8axmbokd38N"
-        )
-        .send({
-          from: learnerAddress[0],
-        });
-      if (nftAward.status) {
-        setMintComplete(true);
-        setMinting(false);
-      }
-      return nftAward.status;
-    } catch (error) {
-      console.log(error);
-      setMinting(false);
-      return;
-    }
-    return;
-  }
-
   return (
     <div className="flex flex-col space-y-4 items-center w-full p-4 rounded bg-base-200 border border-gray-400">
       {checkIfJourneyComplete(user, learningBits) ? (
@@ -117,12 +45,6 @@ const CurriculumSidebar = ({
       <PopUp
         setOpen={setFinishedJourney}
         open={finishedJourney}
-        awardTokens={awardUser}
-        mintNft={awardNFT}
-        minting={minting}
-        addingTokens={addingTokens}
-        mintComplete={mintComplete}
-        addTokensComplete={addTokensComplete}
         learningJourneyName={learningJourneyName}
       />
       <Curriculum

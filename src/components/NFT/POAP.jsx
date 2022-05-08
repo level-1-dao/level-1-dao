3094f9529cf5a0437a8c0de5c9e825d91cc778cb
Ray KananI <kananiconsultants@gmail.com>
2022-05-08 09:28:13 -0400

    Update POAP.jsx

--- src/components/NFT/POAP.jsx
+++ src/components/NFT/POAP.jsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const POAP = ({ poap }) => {
   return (
-    <div>
-      <h1>POAP</h1>
-      <p>{poap[0].link}</p>
+    <div className="text-center">
+      <Image
+        src="/assets/images/logos/poap.svg"
+        width={100}
+        height={100}
+        className="mx-auto"
+        alt="POAP"
+      />
+      <h1>Your POAP Link</h1>
+      <Link href={poap[0].link}>
+        <a target="_blank">{poap[0].link}</a>
+      </Link>
     </div>
   );
 };

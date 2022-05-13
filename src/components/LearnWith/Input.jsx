fe8f89f2828923377f4bbfb6192f0e1eafbe3fd3
Ray KananI <kananiconsultants@gmail.com>
2022-05-13 18:01:56 -0400

    Update Input.jsx
    issue POAP if available when a user shares their reflection

--- src/components/LearnWith/Input.jsx
+++ src/components/LearnWith/Input.jsx
@@ -3,12 +3,15 @@ import { useMutation } from "../../lib/apollo";
 import { useQuery, useSubscription } from "@apollo/client";
 import { useRouter } from "next/router";
 import LearningMomentContainer from "./LearningMomentContainer";
+import { BadgeCheckIcon } from "@heroicons/react/solid";
+
 import {
   ADD_LEARNING_MOMENT,
   GET_USER,
   SUBSCRIBE_USER_LEARNING_MOMENTS,
   SUBSCRIBE_TO_POAP_AVAILABLE,
   SUBSCRIBE_TO_POAPS_AVAILABLE_COUNT,
+  UPDATE_POAP,
 } from "../../lib/graphql";
 import { POAP } from "../NFT";
 
@@ -27,13 +30,20 @@ const Input = ({ learningBitId, learningPromptId, promptType }) => {
       learningPromptId: learningPromptId,
     },
   });
-  const { data: poapsAvailableCount, loading: poapsAvailableCountLoading } =
-    useSubscription(SUBSCRIBE_TO_POAPS_AVAILABLE_COUNT, {
-      variables: {
-        learningPromptId: learningPromptId,
-      },
-    });
+  const {
+    data: poapsAvailableAggregate,
+    loading: poapsAvailableAggregateLoading,
+  } = useSubscription(SUBSCRIBE_TO_POAPS_AVAILABLE_COUNT, {
+    variables: {
+      learningPromptId: learningPromptId,
+    },
+  });
   const user = data?.user_private[0];
+  const poap = poapAvailable?.poaps[0].id;
+  const poapsAvailableCount =
+    poapsAvailableAggregate?.poaps_aggregate.aggregate.count;
+
+  console.log(poap);
 
   const handleLogIn = () => {
     router.push("/api/auth/login?returnTo=" + router.asPath);
@@ -85,6 +95,21 @@ const Input = ({ learningBitId, learningPromptId, promptType }) => {
   };
 
   const {
+    load: updatePoap,
+    loading: updatePoapLoading,
+    error: updatePoapError,
+  } = useMutation(UPDATE_POAP, {
+    onCompleted: (data) => {
+      console.log("POAP updated ", data);
+      return;
+    },
+    onError: (errorContinueLevel) => {
+      console.log("POAP update error :(", errorContinueLevel);
+      return;
+    },
+  });
+
+  const {
     load: addLearningMoment,
     loading: addLearningMomentLoading,
     error: addLearningMomentError,
@@ -92,6 +117,16 @@ const Input = ({ learningBitId, learningPromptId, promptType }) => {
     onCompleted: (data) => {
       // TODO - show alert/toast
       console.log("Learning moment saved ", data);
+      // if POAP available, update POAP
+      if (poap) {
+        updatePoap({
+          variables: {
+            id: poap,
+            learningMomentId: data.insert_learningMoments.returning[0].id,
+            userId: user?.userId,
+          },
+        });
+      }
       checkForUsersLearningMoment(user, learningBitId, learningPromptId);
       setValue("");
       return;
@@ -156,48 +191,67 @@ const Input = ({ learningBitId, learningPromptId, promptType }) => {
           </div>
         </div>
       ) : (
-        <form action="#" className="space-y-8" onSubmit={handleSubmit}>
-          <div className="bg-base-100 relative text-xl border border-gray-300 rounded-lg shadow-sm overflow-hidden group focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
-            <div className="p-6">
-              <label htmlFor="description" className="sr-only">
-                Learning moment input
-              </label>
-              <textarea
-                rows={2}
-                name="description"
-                id="description"
-                className="textarea textarea-ghost w-full text-xl"
-                placeholder="Add your reflection..."
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                disabled={!user}
-              />
-            </div>
+        <>
+          <form action="#" className="space-y-8" onSubmit={handleSubmit}>
+            <div className="bg-base-100 relative text-xl border border-gray-300 rounded-lg shadow-sm overflow-hidden group focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+              <div className="p-6">
+                <label htmlFor="description" className="sr-only">
+                  Learning moment input
+                </label>
+                <textarea
+                  rows={2}
+                  name="description"
+                  id="description"
+                  className="textarea textarea-ghost w-full text-xl"
+                  placeholder="Add your reflection..."
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  disabled={!user}
+                />
+              </div>
 
-            <div className="bottom-0 inset-x-px">
-              <div className="border-t border-gray-200 px-2 py-3 flex items-center justify-end space-x-3 sm:px-3 group-focus-within:border-indigo-500 group-focus-within:ring-1 group-focus-within:ring-indigo-500">
-                <div className="">
-                  {!loading && user ? (
-                    <button
-                      type="submit"
-                      className={`btn btn-primary btn-md ${
-                        addLearningMomentLoading && "loading"
-                      }`}
-                    >
-                      {!addLearningMomentLoading
-                        ? "Add reflection"
-                        : "Adding..."}
-                    </button>
-                  ) : (
-                    <button type="submit" className="btn btn-primary btn-md">
-                      Log-in to add reflection
-                    </button>
-                  )}
+              <div className="bottom-0 inset-x-px">
+                <div className="border-t border-gray-200 px-2 py-3 flex items-center justify-end space-x-3 sm:px-3 group-focus-within:border-indigo-500 group-focus-within:ring-1 group-focus-within:ring-indigo-500">
+                  <div className="">
+                    {!loading && user ? (
+                      <button
+                        type="submit"
+                        className={`btn btn-primary btn-md ${
+                          addLearningMomentLoading && "loading"
+                        }`}
+                      >
+                        {!addLearningMomentLoading
+                          ? "Add reflection"
+                          : "Adding..."}
+                      </button>
+                    ) : (
+                      <button type="submit" className="btn btn-primary btn-md">
+                        Log-in to add reflection
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+          {poapsAvailableCount > 1 && (
+            <div className="rounded-lg bg-purple-100 p-4 mt-2">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <BadgeCheckIcon
+                    className="h-5 w-5 text-base"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="ml-3 flex-1 md:flex md:justify-between">
+                  <p className="text-sm text-base">
+                    POAP currently available for this reflection
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </>
   );
